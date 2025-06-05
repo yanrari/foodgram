b4561c345fea998bf08462e4d34600a3ff216e00
Yana Shulga <yanaup924@gmail.com>
2025-06-05 16:37:05 +0300

    modified:   backend/users/models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=254,
+        unique=True,
+        help_text='Введите адрес электронной почты'
+    )
+    username = models.CharField(
+        'Логин',
+        max_length=150,
+        unique=True,
+        help_text='Введите логин'
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150,
+        help_text='Введите имя'
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150,
+        help_text='Введите фамилию'
+    )
+    avatar = models.ImageField(
+        'Аватар',
+        upload_to='avatars/',
+        null=True,
+        blank=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
