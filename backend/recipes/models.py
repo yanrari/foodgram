8fdbd9732dc4d1d06e65978c3789e6b4f34009e4
Yana Shulga <yanaup924@gmail.com>
2025-06-05 16:26:11 +0300

    modified:   backend/recipes/models.py

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 
 class Ingredient(models.Model):
@@ -19,4 +20,81 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор',
+        help_text='Введите автора'
+    )
+    name = models.CharField(
+        'Название рецепта',
+        max_length=200,
+        help_text='Введите название рецепта'
+    )
+    image = models.ImageField(
+        'Изображение',
+        upload_to='recipes/',
+        help_text='Загрузите изображение'
+    )
+    text = models.TextField(
+        'Описание',
+        help_text='Опишите рецепт'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        verbose_name='Ингредиенты'
+    )
+    cooking_time = models.PositiveIntegerField(
+        'Время приготовления',
+        help_text='Укажите время приготовления в минутах'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients',
+        verbose_name='Рецепт'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='ingredients_list',
+        verbose_name='Ингредиент'
+    )
+    amount = models.PositiveIntegerField(
+        'Количество',
+        default=1,
+        help_text='Укажите количество ингредиента'
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент для рецепта'
+        verbose_name_plural = 'Ингредиенты для рецепта'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'ingredient'],
+                name='unique_recipe_ingredient'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.recipe} - {self.ingredient} ({self.amount})'
