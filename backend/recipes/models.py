6d1c1cc40648188a8e20b23d3ce223b0669bfb15
Yana Shulga <yanaup924@gmail.com>
2025-06-05 16:19:13 +0300

    modified:   backend/recipes/models.py

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=200,
+        help_text='Введите название ингредиента'
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения',
+        max_length=60,
+        help_text='Введите единицу измерения'
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
+    
